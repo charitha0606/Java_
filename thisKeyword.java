0399b7c253b074610b77eabd66e2c4e875b43186
charitha06 <66995002+charitha0606@users.noreply.github.com>
2024-07-02 17:35:03 +0530

    Add files via upload

---
+++ thisKeyword.java
@@ -0,0 +1,13 @@
+class This {
+    int i;
+    This(int i){
+        this.i=i;
+    }
+}
+    public class thisKeyword {
+        public static void main(String[] args){
+            This number=new This(20);
+            System.out.println("Print the Number of i :"+number.i);
+
+        }
+}
